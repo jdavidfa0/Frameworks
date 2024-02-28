05a33de49e7252ccaaa22e421c3addfa3769b812
Jdavidfa0 <jdavidfa0@gmail.com>
2024-02-28 09:53:46 -0500

    AHHHHHHHH

--- app/app.py
+++ app/app.py
@@ -1,18 +1,41 @@
 from flask import Flask, render_template
+import mysql.connector
 
-app=Flask(__name__)
+app = Flask(__name__)
 
-@app.route('/')
+
+db = mysql.connector.connect(
+    host="localhost", user="root", password="", database="agenda"
+)
+
+cursor = db.cursor()
+
+
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
 
-@app.route('/saludo')
-def saludo():
-    return render_template('Registrar.html')
+@app.route("/Registrar", methods=["POST"])
+def registrar_usuario():
+    nombre = (request.form["Nombreper"],)
+    apellido = (request.form["apellidoper"],)
+    correo = (request.form["emailper"],)
+    dirrecion = (request.form["direccionper"],)
+    telefono = (request.form["telefonoper"],)
+    usuario = (request.form["usuarioper"],)
+    contrasena = request.form["contraper"]
+
+    # insertar datos a la tabla persona
+    cursor.execute(
+        "insert into personas (Nombreper,apellidoper,emailper,direccionper,telefonoper,usuarioper,contraper)values(%s,%s,%s,%s,%s,%s,%s)",
+        (nombre, apellido, correo, dirrecion, telefono, usuario, contrasena),
+    )
+    db.commit()
+
+    return redirect(url_for("Registrar"))
 
 
-if __name__ == '__main__':
-    app.add_url_rule('/', view_func=index)
+if __name__ == "__main__":
+    app.add_url_rule("/", view_func=index)
     app.run(debug=True, port=5005)
-
